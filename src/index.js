1b17999f2befa8653f5b0a0c9881bf756f2ae09b
Nikita-2406 <89524208686@mail.ru>
2024-05-01 00:44:51 +0300

    1.1.0

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@
 
 export default function sorting(object) {
   let flag = false;
-  // console.log(object.length)
   for (let i = 0; i < object.length; i++) {
     if (flag) {
       break;
     }
     flag = true;
     for (let j = i + 1; j < object.length; j++) {
-      console.log(object[i].health, object[j].health);
       if (object[i].health < object[j].health) {
         let t = object[i];
         object[i] = object[j];
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,53 +1,69 @@
-import sorting from "../../index"
+import sorting from "../../index";
 
-// console.log(111)
-
-// console.log(sorting([
-//       {name: 'мечник', health: 10},
-//       {name: 'маг', health: 100},
-//       {name: 'лучник', health: 80},
-//     ]))
-
-test("test", () => {
-  const res = sorting([
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-  ])
-  expect(res).toEqual([
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
-  ])
-})
-
-dataList = [
+const dataList = [
   [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-  ], 
-  [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 65},
-    {name: 'лучник', health: 80},
-    {name: 'ковбой', health: 20},
-  ], 
-  [
-    {name: 'мечник', health: 10},
-    {name: 'маг', health: 99},
-    {name: 'ковбой', health: 80},
-    {name: 'пехотинец', health: 81},
-    {name: 'лекарь', health: 2},
-  ], 
-  [
-    {name: 'мечник', health: 8},
-    {name: 'маг', health: 91},
-    {name: 'пехотинец', health: 43},
-    {name: 'лекарь', health: 45},
-    {name: 'волшебник', health: 45},
-    {name: 'бабаЯга', health: 87},
-  ]
-]
+    [
+      { name: "мечник", health: 10 },
+      { name: "маг", health: 100 },
+      { name: "лучник", health: 80 },
+    ],
+    [
+      { name: "маг", health: 100 },
+      { name: "лучник", health: 80 },
+      { name: "мечник", health: 10 },
+    ],
+  ],
 
-test.each(dataList)('testing')
+  [
+    [
+      { name: "мечник", health: 10 },
+      { name: "маг", health: 65 },
+      { name: "лучник", health: 80 },
+      { name: "ковбой", health: 20 },
+    ],
+    [
+      { name: "лучник", health: 80 },
+      { name: "маг", health: 65 },
+      { name: "ковбой", health: 20 },
+      { name: "мечник", health: 10 },
+    ],
+  ],
+
+  [
+    [
+      { name: "мечник", health: 10 },
+      { name: "маг", health: 99 },
+      { name: "ковбой", health: 80 },
+      { name: "пехотинец", health: 81 },
+      { name: "лекарь", health: 2 },
+    ],
+    [
+      { name: "маг", health: 99 },
+      { name: "пехотинец", health: 81 },
+      { name: "ковбой", health: 80 },
+      { name: "мечник", health: 10 },
+      { name: "лекарь", health: 2 },
+    ],
+  ],
+  [
+    [
+      { name: "мечник", health: 8 },
+      { name: "маг", health: 91 },
+      { name: "пехотинец", health: 43 },
+      { name: "лекарь", health: 45 },
+      { name: "бабаЯга", health: 87 },
+    ],
+    [
+      { name: "маг", health: 91 },
+      { name: "бабаЯга", health: 87 },
+      { name: "лекарь", health: 45 },
+      { name: "пехотинец", health: 43 },
+      { name: "мечник", health: 8 },
+    ],
+  ],
+];
+
+test.each(dataList)("testingn function sorting", (list, expected) => {
+  const result = sorting(list);
+  expect(result).toEqual(expected);
+});
