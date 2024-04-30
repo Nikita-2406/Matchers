9d632f028277af7d41d806b9a8b2900e86989679
Nikita-2406 <89524208686@mail.ru>
2024-05-01 00:38:49 +0300

    1.0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import './css/style.css';
+// import './css/style.css';
+// import './js/app';
 
-import './js/app';
-
-// TODO: write your code in app.js
+export default function sorting(object) {
+  let flag = false;
+  // console.log(object.length)
+  for (let i = 0; i < object.length; i++) {
+    if (flag) {
+      break;
+    }
+    flag = true;
+    for (let j = i + 1; j < object.length; j++) {
+      console.log(object[i].health, object[j].health);
+      if (object[i].health < object[j].health) {
+        let t = object[i];
+        object[i] = object[j];
+        object[j] = t;
+        flag = false;
+      }
+    }
+  }
+  return object;
+}
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,53 @@
-import sum from '../basic';
+import sorting from "../../index"
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+// console.log(111)
 
-  expect(result).toBe(6);
-});
+// console.log(sorting([
+//       {name: 'мечник', health: 10},
+//       {name: 'маг', health: 100},
+//       {name: 'лучник', health: 80},
+//     ]))
+
+test("test", () => {
+  const res = sorting([
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ])
+  expect(res).toEqual([
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+  ])
+})
+
+dataList = [
+  [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+  ], 
+  [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 65},
+    {name: 'лучник', health: 80},
+    {name: 'ковбой', health: 20},
+  ], 
+  [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 99},
+    {name: 'ковбой', health: 80},
+    {name: 'пехотинец', health: 81},
+    {name: 'лекарь', health: 2},
+  ], 
+  [
+    {name: 'мечник', health: 8},
+    {name: 'маг', health: 91},
+    {name: 'пехотинец', health: 43},
+    {name: 'лекарь', health: 45},
+    {name: 'волшебник', health: 45},
+    {name: 'бабаЯга', health: 87},
+  ]
+]
+
+test.each(dataList)('testing')
